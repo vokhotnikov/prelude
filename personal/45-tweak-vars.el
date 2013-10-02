305b7a17d206a6546a7291f4b95d39a1bd7599e8
Vladimir Okhotnikov <vokhotnikov@gmail.com>
2013-10-02 09:15:10 +0300

    Fix projectile indexing issues

--- personal/45-tweak-vars.el
+++ personal/45-tweak-vars.el
@@ -2,5 +2,10 @@
 
 (setq-default fill-column 120)
 
+;; Switch to native indexing (with cache) in projectile since it does not behave well with nested projects
+
+(setq projectile-indexing-method 'native)
+(setq projectile-enable-caching t)
+
 (provide '45-tweak-vars)
 ;;; 45-tweak-vars.el ends here
