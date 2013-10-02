32e4aef9cb78bee517219e508cb3eb225a0d5d73
Vladimir Okhotnikov <vokhotnikov@gmail.com>
2013-10-02 10:37:25 +0300

    Disabled EVIL for now

--- personal/55-evil-activate.el
+++ personal/55-evil-activate.el
@@ -1,25 +1,25 @@
 ;;; 50-evil-activate.el --- activate EVIL mode
 
-(require 'evil)
-(evil-mode 1)
+;; (require 'evil)
+;; (evil-mode 1)
 
-;;; esc quits
-(define-key evil-normal-state-map [escape] 'keyboard-quit)
-(define-key evil-visual-state-map [escape] 'keyboard-quit)
-(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+;; ;;; esc quits
+;; (define-key evil-normal-state-map [escape] 'keyboard-quit)
+;; (define-key evil-visual-state-map [escape] 'keyboard-quit)
+;; (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+;; (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+;; (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+;; (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+;; (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
-(define-key evil-normal-state-map "\C-y" 'yank)
-(define-key evil-insert-state-map "\C-y" 'yank)
-(define-key evil-visual-state-map "\C-y" 'yank)
-(define-key evil-insert-state-map "\C-e" 'end-of-line)
-(define-key evil-normal-state-map "\C-w" 'evil-delete)
-(define-key evil-insert-state-map "\C-w" 'evil-delete)
-(define-key evil-insert-state-map "\C-r" 'search-backward)
-(define-key evil-visual-state-map "\C-w" 'evil-delete)
+;; (define-key evil-normal-state-map "\C-y" 'yank)
+;; (define-key evil-insert-state-map "\C-y" 'yank)
+;; (define-key evil-visual-state-map "\C-y" 'yank)
+;; (define-key evil-insert-state-map "\C-e" 'end-of-line)
+;; (define-key evil-normal-state-map "\C-w" 'evil-delete)
+;; (define-key evil-insert-state-map "\C-w" 'evil-delete)
+;; (define-key evil-insert-state-map "\C-r" 'search-backward)
+;; (define-key evil-visual-state-map "\C-w" 'evil-delete)
 
 (provide '50-evil-activate)
 ;;; 50-evil-activate.el ends here
